4270f5f0ba6026034f0d2f3147e9d3b6b3f01a8a
gabsferrara <gabrielbarbosa_ferrara@hotmail.com>
2023-09-10 18:14:55 -0300

    finish base EDA

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -14,6 +14,15 @@ func NewEventDispatcher() *EventDispatcher {
 	}
 }
 
+func (ev *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handle(event)
+		}
+	}
+	return nil
+}
+
 func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
 	if _, ok := ed.handlers[eventName]; ok {
 		for _, h := range ed.handlers[eventName] {
@@ -35,6 +44,18 @@ func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) 
 		}
 	}
 	return false
+}
+
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for i, h := range ed.handlers[eventName] {
+			if h == handler {
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
 }
 
 func (ed *EventDispatcher) Clear() {
